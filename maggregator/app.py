f4ac79030eaaf4b35914f08c1affdf2e0430f91b
Roman Kuzmichev <daerdemandt@gmail.com>
2016-08-23 18:33:43 +0300

    Outer logic of providing a feed

--- maggregator/app.py
+++ maggregator/app.py
@@ -1,20 +1,33 @@
-import os
-from flask import Flask
+from flask import Flask, abort, request
 from pprint import pformat
-import config
+from config import parse
+
+config = parse('config.yml')
 
 app = Flask(__name__)
 
 
-@app.route('/marco')
-def marco():
-    return "Polo!"
 
+@app.route('/health')
+def get_health():
+    return "Ok"
+
+#TODO: this is for debugging purposes only, remove
 @app.route('/config')
 def display_config():
-    r = config.parse('config.yml')
+    return pformat(config)
 
-    return pformat(r)
+@app.route('/feeds/<feedname>')
+def get_feed(feedname):
+    if feedname not in config['feeds']:
+        abort(404)
+    # TODO: proper auth
+    user = request.args.get('user')
+    if user not in config['users'] or feedname not in config['users'][user]['feeds']:
+        abort(403)
+    # Poll ondemand sources
+    # query the db
+    return "nothing here yet"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
