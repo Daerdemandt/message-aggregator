9b30e69e36ce7510b65cf86f7536a350a26a146c
Roman Kuzmichev <daerdemandt@gmail.com>
2016-08-25 23:13:49 +0300

    Fixed behaviour when nexmo checks if address is ok

--- maggregator/sources/nexmo.py
+++ maggregator/sources/nexmo.py
@@ -4,6 +4,9 @@ from flask import request
 
 class NexmoSource(WebhookSource):
     def handle(self):
+        if not request.args.get('message-timestamp'):
+            # This is not actually a message, nexmo is just checking if address is OK
+            return ""
         self.save_message(
             request.args.get('text'),
             request.args.get('msisdn'),
